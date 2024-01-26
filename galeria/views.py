73360e92cb66346f22c402535aff646982d9bc1a
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-01-26 00:03:17 -0300

    adicionando vida a pagina, style.css

--- galeria/views.py
+++ galeria/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 #criando funcao principal da aplicação
 def index(request):
-    return render(request, 'index.html')
+    return render(request, 'galeria/index.html')
--- setup/settings.py
+++ setup/settings.py
@@ -121,6 +121,14 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+#indicar onde os arquivos estaticos estao
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'setup/static')
+]
+
+#caminho absoluto onde o python irá coletar os arquivos staticos
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
