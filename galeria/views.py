51f177416d64ce217649df7bd1999f3756178b01
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-01-25 23:26:35 -0300

    Primeiro template html adicionado

--- galeria/views.py
+++ galeria/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 #criando funcao principal da aplicação
 def index(request):
-    return HttpResponse('<h1>Hello World</h1>')
+    return render(request, 'index.html')
