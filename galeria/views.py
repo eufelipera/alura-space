78b011664a81b7b27d4d80fb1d9df702f2df6e7b
Felipe Sousa <felipe.soares2007@hotmail.com>
2024-02-01 00:31:00 -0300

    projeto final, falta melhorar um pouco mais

--- galeria/views.py
+++ galeria/views.py
@@ -3,3 +3,5 @@ from django.shortcuts import render
 #criando funcao principal da aplicação
 def index(request):
     return render(request, 'galeria/index.html')
+def imagem(request):
+    return render(request, 'galeria/imagem.html')
